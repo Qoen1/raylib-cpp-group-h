91ea048ad8334042b76ac36c10b9baba908f1f97
fikkie <k.verstappen1@student.avans.nl>
2024-12-09 12:58:17 +0100

    done.

--- src/rmodels.c
+++ src/rmodels.c
@@ -3591,6 +3591,119 @@ void DrawBillboardPro(Camera camera, Texture2D texture, Rectangle source, Vector
 
     rlSetTexture(0);
 }
+/// @brief draws a texture on a 3d plane. note that this does not handle positioning or rotation.
+/// @param camera: the current camera.
+/// @param texture: the texture to be rendered.
+/// @param source: the source rectangle that defines the part of the texture that needs to be rendered.
+/// @param tint: the tint to display on the texture.
+/// @param faceDirection:the direction in which the drawn texture must face.
+void DrawTextureOnPlane(Camera camera, Texture2D texture, Rectangle source, Vector2 size, Color tint, Vector3 faceDirection) {
+    Vector3 up = {0,1,0};
+    // NOTE: Billboard size will maintain source rectangle aspect ratio, size will represent billboard width
+    Vector2 sizeRatio = { size.x*fabsf((float)source.width/source.height), size.y };
+
+    Matrix matView = MatrixLookAt(faceDirection, camera.target, camera.up);
+
+    Vector3 right = { matView.m0, matView.m4, matView.m8 };
+    //Vector3 up = { matView.m1, matView.m5, matView.m9 };
+
+    Vector3 rightScaled = Vector3Scale(right, sizeRatio.x/2);
+    Vector3 upScaled = Vector3Scale(up, sizeRatio.y/2);
+
+    Vector3 p1 = Vector3Add(rightScaled, upScaled);
+    Vector3 p2 = Vector3Subtract(rightScaled, upScaled);
+
+    Vector3 topLeft = Vector3Scale(p2, -1);
+    Vector3 topRight = p1;
+    Vector3 bottomRight = p2;
+    Vector3 bottomLeft = Vector3Scale(p1, -1);
+
+    // if (rotation != 0.0f)
+    // {
+    //     float sinRotation = sinf(rotation*DEG2RAD);
+    //     float cosRotation = cosf(rotation*DEG2RAD);
+    //
+    //     // NOTE: (-1, 1) is the range where origin.x, origin.y is inside the texture
+    //     float rotateAboutX = sizeRatio.x*origin.x/2;
+    //     float rotateAboutY = sizeRatio.y*origin.y/2;
+    //
+    //     float xtvalue, ytvalue;
+    //     float rotatedX, rotatedY;
+    //
+    //     xtvalue = Vector3DotProduct(right, topLeft) - rotateAboutX; // Project points to x and y coordinates on the billboard plane
+    //     ytvalue = Vector3DotProduct(up, topLeft) - rotateAboutY;
+    //     rotatedX = xtvalue*cosRotation - ytvalue*sinRotation + rotateAboutX; // Rotate about the point origin
+    //     rotatedY = xtvalue*sinRotation + ytvalue*cosRotation + rotateAboutY;
+    //     topLeft = Vector3Add(Vector3Scale(up, rotatedY), Vector3Scale(right, rotatedX)); // Translate back to cartesian coordinates
+    //
+    //     xtvalue = Vector3DotProduct(right, topRight) - rotateAboutX;
+    //     ytvalue = Vector3DotProduct(up, topRight) - rotateAboutY;
+    //     rotatedX = xtvalue*cosRotation - ytvalue*sinRotation + rotateAboutX;
+    //     rotatedY = xtvalue*sinRotation + ytvalue*cosRotation + rotateAboutY;
+    //     topRight = Vector3Add(Vector3Scale(up, rotatedY), Vector3Scale(right, rotatedX));
+    //
+    //     xtvalue = Vector3DotProduct(right, bottomRight) - rotateAboutX;
+    //     ytvalue = Vector3DotProduct(up, bottomRight) - rotateAboutY;
+    //     rotatedX = xtvalue*cosRotation - ytvalue*sinRotation + rotateAboutX;
+    //     rotatedY = xtvalue*sinRotation + ytvalue*cosRotation + rotateAboutY;
+    //     bottomRight = Vector3Add(Vector3Scale(up, rotatedY), Vector3Scale(right, rotatedX));
+    //
+    //     xtvalue = Vector3DotProduct(right, bottomLeft)-rotateAboutX;
+    //     ytvalue = Vector3DotProduct(up, bottomLeft)-rotateAboutY;
+    //     rotatedX = xtvalue*cosRotation - ytvalue*sinRotation + rotateAboutX;
+    //     rotatedY = xtvalue*sinRotation + ytvalue*cosRotation + rotateAboutY;
+    //     bottomLeft = Vector3Add(Vector3Scale(up, rotatedY), Vector3Scale(right, rotatedX));
+    // }
+
+    // Translate points to the draw center (position)
+    // topLeft = Vector3Add(topLeft, position);
+    // topRight = Vector3Add(topRight, position);
+    // bottomRight = Vector3Add(bottomRight, position);
+    // bottomLeft = Vector3Add(bottomLeft, position);
+
+    rlSetTexture(texture.id);
+
+    rlBegin(RL_QUADS);
+        rlColor4ub(tint.r, tint.g, tint.b, tint.a);
+
+        if (sizeRatio.x * sizeRatio.y >= 0.0f)
+        {
+            // Bottom-left corner for texture and quad
+            rlTexCoord2f((float)source.x/texture.width, (float)source.y/texture.height);
+            rlVertex3f(topLeft.x, topLeft.y, topLeft.z);
+
+            // Top-left corner for texture and quad
+            rlTexCoord2f((float)source.x/texture.width, (float)(source.y + source.height)/texture.height);
+            rlVertex3f(bottomLeft.x, bottomLeft.y, bottomLeft.z);
+
+            // Top-right corner for texture and quad
+            rlTexCoord2f((float)(source.x + source.width)/texture.width, (float)(source.y + source.height)/texture.height);
+            rlVertex3f(bottomRight.x, bottomRight.y, bottomRight.z);
+
+            // Bottom-right corner for texture and quad
+            rlTexCoord2f((float)(source.x + source.width)/texture.width, (float)source.y/texture.height);
+            rlVertex3f(topRight.x, topRight.y, topRight.z);
+        }
+        else
+        {
+            // Reverse vertex order if the size has only one negative dimension
+            rlTexCoord2f((float)(source.x + source.width)/texture.width, (float)source.y/texture.height);
+            rlVertex3f(topRight.x, topRight.y, topRight.z);
+
+            rlTexCoord2f((float)(source.x + source.width)/texture.width, (float)(source.y + source.height)/texture.height);
+            rlVertex3f(bottomRight.x, bottomRight.y, bottomRight.z);
+
+            rlTexCoord2f((float)source.x/texture.width, (float)(source.y + source.height)/texture.height);
+            rlVertex3f(bottomLeft.x, bottomLeft.y, bottomLeft.z);
+
+            rlTexCoord2f((float)source.x/texture.width, (float)source.y/texture.height);
+            rlVertex3f(topLeft.x, topLeft.y, topLeft.z);
+        }
+
+    rlEnd();
+
+    rlSetTexture(0);
+}
 
 // Draw a bounding box with wires
 void DrawBoundingBox(BoundingBox box, Color color)
